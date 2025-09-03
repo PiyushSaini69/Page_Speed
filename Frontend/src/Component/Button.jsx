368aa531a311c3785217dcd39c9a7333428850f8
Piyush Saini <piyushsaini1226@gmail.com>
2025-09-03 12:23:30 +0530

    button done

--- Frontend/src/Component/Button.jsx
+++ Frontend/src/Component/Button.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Button({ children }) {
+export default function Button({ children,onClick }) {
 //   const [active, setActive] = useState(false);
 
   return (
    
         <button
+        onClick={onClick}
      className="px-6 py-2 ml-2 mr-2 rounded-md font-medium shadow-lg 
         transition-colors duration-300 focus:outline-none 
         focus:ring-2 focus:ring-blue-400 focus:ring-offset-1 
