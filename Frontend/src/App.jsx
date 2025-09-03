53c581f65d29405182757a5f91d5060aa9ba8e5d
Dheeraj Saini <dheeraj@Dheerajs-MacBook-Air.local>
2025-09-03 10:35:16 +0530

    Type Manually Only

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,10 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Component/Home'
-import File from './Component/File'
+
 // import {BrowserRoutes as Router, Routes ,Route} from 'react-router-dom'
 import {Routes, Route } from "react-router-dom";
-import Button from './Component/Button'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,9 +17,9 @@ function App() {
 
     <Routes>
       
-      <Route path='/' element={<Button/>} />
-      <Route path='/home' element={<Home/>} />
-      <Route path='/file' element={<File/>} />
+      
+      <Route path='/' element={<Home/>} />
+      
     </Routes>
 
     {/* <File/> */}
